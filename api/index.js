a38d0d5e6b5269d1d0258290e2f1ce67a165c322
taekwon <taekwon02@gmail.com>
2026-03-23 21:30:05 +0900

    test: 최소 함수로 Vercel 환경 진단

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-module.exports = require('../index');
+module.exports = (req, res) => {
+  res.json({ status: 'ok', env: {
+    SUPABASE_URL: !!process.env.SUPABASE_URL,
+    SUPABASE_KEY: !!process.env.SUPABASE_KEY,
+    OPENAI_API_KEY: !!process.env.OPENAI_API_KEY,
+    NODE_ENV: process.env.NODE_ENV,
+  }});
+};
